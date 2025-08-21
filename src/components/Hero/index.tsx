e77942a41e512ca7553318dff236edc8475d8533
santyasm <yyasant5@gmail.com>
2025-08-21 19:34:43 -0300

    styles: responsive

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -31,9 +31,9 @@ export const Hero = () => {
     return () => clearTimeout(timeout);
   }, [text, isDeleting, index]);
   return (
-    <div className="flex items-center justify-between w-full px-[9%] relative h-screen text-center md:text-start">
-      <div>
-        <h1 className="text-white font-bold text-4xl lg:text-[80px] animate-fade-right animate-duration-500 animate-delay-300 animate-ease-in">
+    <div className="flex lg:flex-row flex-col w-full px-[9%] items-stretch min-h-screen h-auto justify-center pt-24">
+      <div className="text-center md:text-start md:mt-[22%] lg:mt-0 flex-1">
+        <h1 className="text-white font-bold text-4xl md:text-[70px] lg:text-[80px] animate-fade-right animate-duration-500 animate-delay-300 animate-ease-in">
           Yasmin Santana
         </h1>
 
@@ -56,22 +56,24 @@ export const Hero = () => {
           </motion.span>
         </div>
 
-        <p className="text-whitetext text-[18px] leading-8 mt-6 lg:w-[45%] font-inter">
+        <p className="text-whitetext md:text-[18px] leading-7 mt-6 lg:w-[45%] font-inter w-[96%]">
           Desenvolvedora front-end com experiência em React, React Native e
           TypeScript, atuando em TI desde 2022. Iniciei como Analista de Suporte
           Técnico, destacando-me pela resolução de problemas complexos e
           aprendizado rápido, o que abriu
         </p>
 
-        <button className="bg-gradient-to-tr cursor-pointer from-darksky via-[#848AD4] to-slightly rounded-full py-2 px-6 text-white font-semibold text-[18px] mt-10 animate-fade-right hover:animate-shake animate-duration-500 animate-delay-300 animate-ease-in">
+        <button className="bg-gradient-to-tr cursor-pointer from-darksky via-[#848AD4] to-slightly rounded-full py-2 px-6 text-white font-semibold text-[18px] mt-10 animate-fade-right animate-duration-500 animate-delay-300 animate-ease-in">
           Contato
         </button>
       </div>
 
       <img
         src="/yasmin.png"
-        className="w-[360px] md:w-[50vh] lg:w-[40vw] absolute self-center bottom-0 md:right-10 animate-fade-up animate-duration-500 animate-delay-300 animate-ease-in"
+        className="w-[70vw] mx-auto md:mr-0 md:w-[60vw] lg:w-[40vw] lg:absolute lg:bottom-0 lg:right-10 h-auto animate-fade-up animate-duration-500 animate-delay-300 animate-ease-in"
         alt=""
+        data-blobity-tooltip="Hello Guys"
+        data-blobity-invert="false"
       />
     </div>
   );
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -5,13 +5,14 @@ import { Header } from "../Header";
 export const Home = () => {
   return (
     <motion.section
-      className="relative z-10 flex flex-col min-h-screen w-full items-stretch bg-[url('.//../public/hero.png')] bg-cover bg-center py-0 rounded-t-[25px]"
+      className="relative z-10 flex flex-col w-full min-h-screen h-auto bg-[url('.//../public/hero.png')] bg-cover bg-center py-0 rounded-t-[25px]"
       id="home"
       initial="initial"
       animate="animate"
     >
       <Header />
       <Hero />
+
     </motion.section>
   );
 };
