4f238cc136219f28e2616c42ad6c385f2a1517d2
santyasm <yyasant5@gmail.com>
2025-08-20 21:48:11 -0300

    feat: add blobity

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import { Home } from "./components/Home"
+import { useEffect } from "react";
+import { Home } from "./components/Home";
+import useBlobity from "blobity/lib/react/useBlobity";
+import { initialBlobityOptions } from "./utils/BlobityConfig";
 
 function App() {
-  return (
-    <Home />
-  )
+  const blobityInstance = useBlobity(initialBlobityOptions);
+
+  useEffect(() => {
+    if (blobityInstance.current) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      window.blobity = blobityInstance.current;
+    }
+  }, [blobityInstance]);
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  }, []);
+  return <Home />;
 }
 
-export default App
-  
+export default App;
