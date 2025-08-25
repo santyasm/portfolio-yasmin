dcfa5163fd79217a7c30d16819c6101f85926e65
santyasm <yyasant5@gmail.com>
2025-08-25 12:02:38 -0300

    chore: add SEO and social meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Projects } from "./components/Projects";
 import Footer from "./components/Footer";
 import { Contact } from "./components/Contact";
 import { Analytics } from "@vercel/analytics/react";
+import { Helmet } from "react-helmet";
 
 function App() {
   const blobityInstance = useBlobity(initialBlobityOptions);
@@ -27,6 +28,26 @@ function App() {
   }, []);
   return (
     <>
+        <Helmet>
+        <title>Yasmin Santana — Frontend & Mobile Developer</title>
+        <meta
+          name="description"
+          content="Desenvolvedora Front End e Mobile. Focada em interfaces e experiências."
+        />
+        <meta name="keywords" content="freelance, developer, frontend, react, frontend developer portfolio, mobile developer, mobile" />
+        <meta name="application-name" content="Yasmin Santana" />
+        <meta name="robots" content="index, follow" />
+        <meta name="color-scheme" content="dark" />
+
+        {/* Open Graph */}
+        <meta property="og:title" content="Yasmin Santana — Frontend & Mobile Developer" />
+        <meta property="og:description" content="Desenvolvedora Front End e Mobile. Focada em interfaces e experiências." />
+        <meta property="og:url" content="https://www.yasminsantana.me/" />
+        <meta property="og:site_name" content="www.yasminsantana.me" />
+        <meta property="og:image" content="https://raw.githubusercontent.com/santyasm/portfolio-yasmin/main/public/yasmin.png" />
+        <meta property="og:type" content="website" />
+      </Helmet>
+
       <Home />
       <About />
       <Projects />
