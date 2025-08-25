e554a3dfde6bed620bd03af9dae2ee52b2927cf6
santyasm <yyasant5@gmail.com>
2025-08-25 11:38:18 -0300

    styles: shadow home

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -7,8 +7,11 @@ export const Home = () => {
     <motion.section
       className="relative flex flex-col w-full min-h-screen h-auto
     rounded-t-[25px]
-    bg-[url('/hero.png')] bg-cover bg-center
-    shadow-amber-50"
+    bg-[url('/hero.png')] bg-cover bg-top
+     mb-10 
+     after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-6
+    after:shadow-[0_10px_20px_2px_#352553] after:rounded-t-[25px]
+     "
       id="home"
       initial="initial"
       animate="animate"
